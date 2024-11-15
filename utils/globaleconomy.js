1704c77471d10c5974f947e5cacc97d68d075029
Alin Straps <strapples.ethermail@ud.me>
2024-11-15 17:22:32 -0600

    remove all to prepare for rebase...

--- utils/globaleconomy.js
+++
@@ -1,6 +0,0 @@
-// utils/globaleconomy.js
-const cron = require('node-cron');
-const { adjustTollRates } = require('../controllers/globaleconomycontroller');
-
-// Adjust toll rates daily
-cron.schedule('0 0 * * *', adjustTollRates); // Every day at midnight
